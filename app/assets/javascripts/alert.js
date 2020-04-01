6430baf98358f199d73afa95437d79b989abde91
Amadou Ibrahim <amadou140589@gmail.com>
2020-04-01 05:41:15 +0100

    Alert.js edited

--- app/assets/javascripts/alert.js
+++ app/assets/javascripts/alert.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
     var $alert = Array.prototype.slice.call(document.querySelectorAll('.notification .delete'), 0);
       if ($alert.length > 0) {  
-      $alert.forEach( el => {
+      $alert.forEach( function(el){
         el.addEventListener('click', function(event){
             var target = event.target;
           var $target = target.parentElement;  

5b05db3187c6dc28f0c63f64d606a38dbcf2ffd8
Amadou Ibrahim <amadou140589@gmail.com>
2020-04-06 13:31:01 +0100

    Like Model test edited

--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -5,22 +5,29 @@ RSpec.describe Like, type: :model do
   let(:post) { Post.new(content: 'A simple post content', user_id: user.id) }
   let(:like) { Like.new(post_id: post.id, user_id: user.id) }
 
-  context 'it has post_id and user_id' do
-    it 'has to have a post_id' do
-      expect(like.post_id).to eq(post.id)
+  describe 'Validations' do 
+    context 'it has post_id and user_id' do
+      it 'has to have a post_id' do
+        expect(like.post_id).to eq(post.id)
+      end
+      it 'has to have a user_id' do
+        expect(like.user_id).to eq(user.id)
+      end
     end
-    it 'has to have a user_id' do
-      expect(like.user_id).to eq(user.id)
+    context 'it has to return an error for missing fields' do
+      it 'returns an error for missing post_id' do
+        like.post_id = nil
+        expect(like.save).to eq(false)
+      end
+      it 'returns an error for missing user_id' do
+        like.user_id = nil
+        expect(like.save).to eq(false)
+      end
     end
   end
-  context 'it has to return an error for missing fields' do
-    it 'returns an error for missing post_id' do
-      like.post_id = nil
-      expect(like.save).to eq(false)
-    end
-    it 'returns an error for missing user_id' do
-      like.user_id = nil
-      expect(like.save).to eq(false)
-    end
+  describe 'Associations' do    
+    it { should belong_to(:user) }
+    it { should belong_to(:post) }
   end
+  
 end

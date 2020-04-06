36c47206822765b85607fd9e9db8f4bbc10f6560
Amadou Ibrahim <amadou140589@gmail.com>
2020-04-06 13:32:14 +0100

    Friendship Model test edited

--- spec/models/friendship_spec.rb
+++ spec/models/friendship_spec.rb
@@ -5,18 +5,25 @@ RSpec.describe Friendship, type: :model do
   let(:user2) { User.create(name: 'JaneDoe', email: 'janedoe@ymail.com', password: 'password') }
   let(:friendship) { Friendship.new(user_id: user1.id, friend_id: user2.id) }
 
-  it 'has to have a user_id' do
-    expect(friendship.user_id).to eq(user1.id)
+  describe 'Validations' do
+    it 'has to have a user_id' do
+      expect(friendship.user_id).to eq(user1.id)
+    end
+    it 'has to have a friend_id' do
+      expect(friendship.friend_id).to eq(user2.id)
+    end
+    it 'returns an error for missing user_id' do
+      friendship.user_id = nil
+      expect(friendship.save).to eq(false)
+    end
+    it 'returns an error for missing friend_id' do
+      friendship.friend_id = nil
+      expect(friendship.save).to eq(false)
+    end
   end
-  it 'has to have a friend_id' do
-    expect(friendship.friend_id).to eq(user2.id)
-  end
-  it 'returns an error for missing user_id' do
-    friendship.user_id = nil
-    expect(friendship.save).to eq(false)
-  end
-  it 'returns an error for missing friend_id' do
-    friendship.friend_id = nil
-    expect(friendship.save).to eq(false)
+
+  describe 'Associations' do    
+    it { should belong_to(:user) }
+    it { should belong_to(:friend) }
   end
 end
